729e66870b4a06cfbdbc950e64c70fe7c6ad9a28
mrugankthatte55 <mrugankthatte@gmail.com>
2022-07-20 14:31:13 +0530

    contact

--- ecommerce-frontend/ecommerce-frontend/src/components/ContactUs.jsx
+++ ecommerce-frontend/ecommerce-frontend/src/components/ContactUs.jsx
@@ -16,9 +16,25 @@ function ContactUs() {
         <div style={{display:"grid"}}><p style={{ color: "white"}}>
           <a style={{fontSize:"30px"}}>Feel free to reach out to us at any of the following!</a>
           <br />
+          <br />
           <div>
-          <img src={'email.png'} style={{width:'33px', paddingRight:'2px',paddingTop:"0px"}} />
+          <img src={'email2.png'} style={{width:'33px', paddingRight:'2px',paddingTop:"0px"}} />
           <a style={{fontSize:"22px",paddingTop:"2px"}}>Email-Id: abcd@dummy.com</a>
+          </div>
+          <br />
+          <div>
+          <img src={'address.png'} style={{width:'33px', paddingRight:'2px',paddingTop:"0px"}} />
+          <a style={{fontSize:"22px",paddingTop:"2px"}}>Address: Building alpha, 3rd Floor, X street, ABC</a>
+          </div>
+          <br />
+          <div>
+          <img src={'landline2.png'} style={{width:'33px', paddingRight:'2px',paddingTop:"0px"}} />
+          <a style={{fontSize:"22px",paddingTop:"2px"}}>Contact No: 123345678</a>
+          </div>
+          <br />
+          <div>
+          <img src={'cellphone1.png'} style={{width:'33px', paddingRight:'2px',paddingTop:"0px"}} />
+          <a style={{fontSize:"22px",paddingTop:"2px"}}>Contact No(Mobile): 11111111</a>
           </div>
         </p></div>
         
