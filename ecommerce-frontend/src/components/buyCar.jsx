28ecdd4ef798efbf30731f790762c3d89c21e14b
Suhani Isarka <suhaniisarka2607@gmail.com>
2022-07-21 21:20:39 +0530

    commit

--- ecommerce-frontend/src/components/buyCar.jsx
+++ ecommerce-frontend/src/components/buyCar.jsx
@@ -1,169 +1,116 @@
+import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import ReactPaginate from "react-paginate";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useParams, useLocation } from "react-router-dom";
-import Product from "./Product";
-import queryString from "query-string";
-import TopSlider from "./TopSlider";
+import { Link } from "react-router-dom";
 
-function Home() {
+function BuyCar() {
+  const sellerid = sessionStorage.getItem("id");
   const [products, setProducts] = useState([]);
-  const [totalPage, setTotalPage] = useState(0);
-  const state = useSelector((state) => state);
-  const location = useLocation();
-  const [item, setItem] = useState({});
-  const [qty, setQty] = useState(1);
-  const dispatch = useDispatch();
-  const history = useHistory();
-
-  const [showDialog, setShowDialog] = useState("modal fade");
-  const [display, setDisplay] = useState("none");
-
-  const showModal = (prod) => {
-    setShowDialog("modal fade show");
-    setDisplay("block");
-    setItem(prod);
-  };
-
-  const checkItem = (prodid) => {
-    return state.cart.findIndex((x) => x.prodid === prodid) < 0;
-  };
-
-  const closeDialog = () => {
-    setShowDialog("modal fade");
-    setDisplay("none");
-  };
-
-  const loadDataFromServer = (page = 0, pagesize = 8) => {
+  useEffect(() => {
     axios
-      .get(
-        "http://localhost:4000/api/products/paginated?page=" +
-          page +
-          "&pagesize=" +
-          pagesize
-      )
+      .get("http://localhost:4000/api/products?sellerid=" + sellerid)
       .then((resp) => {
-        console.log(resp.data.data.total);
-        setProducts(resp.data.data.plist);
-        setTotalPage(Math.ceil(resp.data.data.total / pagesize));
+        console.log(resp.data);
+        setProducts(resp.data.data);
         console.log(products);
       });
-  };
+  }, []);
 
-  useEffect(() => {
-    console.log("I am here cat", location.search);
-    let pcat = queryString.parse(location.search);
-    console.log(pcat.cat);
-    if (pcat.cat !== undefined) {
+  const deleteProduct = (prodid) => {
+    let resp = window.confirm("Are you sure to delete this product ?");
+    if (resp) {
       axios
-        .get("http://localhost:4000/api/products?cat=" + pcat.cat)
+        .delete("http://localhost:4000/api/products/" + prodid)
         .then((resp) => {
-          console.log(resp.data);
-          setProducts(resp.data.data);
-          console.log(products);
+          alert("Product deleted successfully");
+          axios
+            .get("http://localhost:4000/api/products?sellerid=" + sellerid)
+            .then((resp) => {
+              console.log(resp.data);
+              setProducts(resp.data.data);
+              console.log(products);
+            });
         });
-    } else {
-      loadDataFromServer();
     }
-  }, [location]);
-  const addToCart = (item) => {
-    if (sessionStorage.getItem("userid") == null) {
-      alert("Please login first to buy product");
-      history.push("/clogin");
-    } else if (sessionStorage.getItem("role") !== "customer") {
-      alert("Only customer can buy product");
-    } else {
-      if (checkItem(item.prodid)) {
-        showModal();
-        setDisplay("none");
-        setShowDialog("modal fade");
-        item.qty = qty;
-        dispatch({ type: "AddItem", payload: item });
-        alert("Item added to cart successfully");
-      } else {
-        alert("Item already in cart");
-      }
-    }
-  };
-
-  const handlePageClick = ({ selected: selectedPage }) => {
-    loadDataFromServer(selectedPage);
   };
 
   return (
-    <>
-      <div className="container-fluid" style={{ width: "92%" }}>
-        <div className="card shadow bg-transparent">
-          <div className="card-body">
-            <ReactPaginate
-              previousLabel={""}
-              nextLabel={""}
-              containerClassName={"pagination"}
-              pageCount={totalPage}
-              onPageChange={handlePageClick}
-              previousLinkClassName={"pagination__link"}
-              nextLinkClassName={"pagination__link"}
-              disabledClassName={"pagination__link--disabled"}
-              activeClassName={"pagination__link--active"}
-            />
-            <div className="row">
+    <div className="container" style={{ paddingTop: "50px" }}>
+      <div className="card shadow bg-transparent text-white">
+        <div className="card-body">
+          <h4 style={{ fontSize: "30px" }}>My Products</h4>
+          <table className="table table-bordered">
+            <thead className="table-light">
+              <tr>
+                <th style={{ border: "2px solid black" }}>
+                  <a style={{ fontSize: "25px" }}>Name</a>
+                </th>
+                <th style={{ border: "2px solid black" }}>
+                  <a style={{ fontSize: "25px" }}>Brand Name</a>
+                </th>
+                <th style={{ border: "2px solid black" }}>
+                  <a style={{ fontSize: "25px" }}>Category</a>
+                </th>
+                <th style={{ border: "2px solid black" }}>
+                  <a style={{ fontSize: "25px" }}>Price</a>
+                </th>
+                <th style={{ border: "2px solid black" }}>
+                  <a style={{ fontSize: "25px" }}>Action</a>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
               {products.map((x) => (
-                <Product key={x.prodid} x={x} showModal={showModal} />
-              ))}
-            </div>
-          </div>
-        </div>
-        {display == "block" ? (
-          <div
-            className={showDialog}
-            style={{ zIndex: "1000", display: display }}
-          >
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5>Add to Cart</h5>
-                  <button onClick={closeDialog} className="close">
-                    &times;
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <div className="d-flex">
+                <tr key={x.prodid}>
+                  <td style={{ textAlign: "center", verticalAlign: "middle" }}>
                     <img
-                      src={"http://localhost:4000/" + item.photo}
-                      style={{ width: "200px" }}
+                      width="100"
+                      src={"http://localhost:4000/" + x.photo}
+                      className="img-thumnail"
                     />
-                    <div className="ml-3">
-                      <h4 className="p-2 text-warning">{item.pname}</h4>
-                      <h5 className="px-2">Brand: {item.brand}</h5>
-                      <h5 className="px-2">Category: {item.pcat}</h5>
-                      <h5 className="px-2">Seller: {item.sellerName}</h5>
-                      <h5 className="px-2">Price: &#8377; {item.price}</h5>
-                      <input
-                        type="number"
-                        value={qty}
-                        onChange={(e) => setQty(e.target.value)}
-                      />
+                    <a
+                      style={{
+                        color: "white",
+                        paddingLeft: "20px",
+                        fontSize: "20px",
+                      }}
+                    >
+                      {x.pname}
+                    </a>
+                  </td>
+                  <td style={{ textAlign: "center", verticalAlign: "middle" }}>
+                    <a style={{ color: "white", fontSize: "20px" }}>{x.pcat}</a>
+                  </td>
+                  <td style={{ textAlign: "center", verticalAlign: "middle" }}>
+                    <a style={{ color: "white", fontSize: "20px" }}>
+                      {x.brand}
+                    </a>
+                  </td>
+                  <td style={{ textAlign: "center", verticalAlign: "middle" }}>
+                    <a style={{ color: "white", fontSize: "20px" }}>
+                      {x.price.toLocaleString(undefined, {
+                        maximumFractionDigits: 2,
+                      })}
+                    </a>
+                  </td>
+                  <td style={{ verticalAlign: "middle" }}>
+                    <div className="container center">
+                      <button
+                        onClick={() => deleteProduct(x.prodid)}
+                        className="btn btn-danger btn-sm"
+                      >
+                        Add To Cart
+                      </button>
                     </div>
-                  </div>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    onClick={(e) => addToCart(item)}
-                    className="btn btn-warning btn-sm"
-                  >
-                    Add to Cart
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default Home;
+export default BuyCar;
